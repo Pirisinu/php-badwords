d8431e62425e885791fcede2e6ab06e691a772f1
Pirisinu <palazzialessandro2@gmail.com>
2023-10-25 15:17:00 +0200

    [INIT]Repo

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$saluto = 'Ciao da variabile';
+
+echo'ciao da Php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Php Badwords</title>
+</head>
+<body>
+  <h1>Ciao da Html</h1>
+  <h2><?php echo $saluto ?></h2>
+</body>
+</html>
