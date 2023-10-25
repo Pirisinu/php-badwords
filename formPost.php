f4d3e601a95f907629a9392590c35384b6fe5aef
Pirisinu <palazzialessandro2@gmail.com>
2023-10-25 17:06:56 +0200

    [ADD]$paragrafoCensuratoLength

--- formPost.php
+++ formPost.php
@@ -6,13 +6,14 @@ $censoredWord = array(
   'cane',
   'pezzo',
 );
-//Paragrafo concatenato
+//Paragrafo concatenato con length
 $paragrafoLength = 'Il paragrafo ha ' . strlen($paragrafo) . ' caratteri nella parola';
 //Array di parole vietate concatenate
 $censoredWordJoin = implode(', ', $censoredWord);
 //Paragrafo Censurato
 $paragrafoCensurato = str_replace($censoredWord, '***', $paragrafo);
-
+//Paragrafo Censurato concatenato con length
+$paragrafoCensuratoLength = 'Il paragrafo ha ' . strlen($paragrafoCensurato) . ' caratteri nella parola';
 ?>
 
 <!DOCTYPE html>
@@ -26,21 +27,25 @@ $paragrafoCensurato = str_replace($censoredWord, '***', $paragrafo);
 <body>
   <h1 class="my-3">FORM-SIDE</h1>
   <ul class="list-group">
-    <li class="list-group-item list-group-item-action list-group-item-primary">
-      <h2>Paragrafo in entrata:</h2>
-      <h3><?php echo $paragrafo ?></h3>
-    </li>
-    <li class="list-group-item list-group-item-action list-group-item-secondary">
-      <h2>Paragrafo editato:</h2>
-      <h3><?php echo $paragrafoLength ?></h3>
-    </li>
-    <li class="list-group-item list-group-item-action list-group-item-success">
-      <h2>Parole da censurare:</h2>
+    <li class="list-group-item list-group-item-action list-group-item-danger text-center">
+      <h2 class="mb-5">Parole da censurare:</h2>
       <h3><?php echo $censoredWordJoin ?></h3>
     </li>
-    <li class="list-group-item list-group-item-action list-group-item-danger">
-      <h2>Paragrafo censurato:</h2>
+    <li class="list-group-item list-group-item-action list-group-item-primary text-center">
+      <h2 class="mb-5">Paragrafo in entrata:</h2>
+      <h3><?php echo $paragrafo ?></h3>
+    </li>
+    <li class="list-group-item list-group-item-action list-group-item-secondary text-center">
+      <h2 class="mb-5">Lunghezza paragrafo originale:</h2>
+      <h3><?php echo $paragrafoLength ?></h3>
+    </li>
+    <li class="list-group-item list-group-item-action list-group-item-warning text-center">
+      <h2 class="mb-5">Paragrafo censurato:</h2>
       <h3><?php echo $paragrafoCensurato ?></h3>
+    </li>
+    <li class="list-group-item list-group-item-action list-group-item-secondary text-center">
+      <h2 class="mb-5">Lunghezza paragrafo censurato:</h2>
+      <h3><?php echo $paragrafoCensuratoLength ?></h3>
     </li>
   </ul>
   
